03d9a782fdfe64b19c9a235d78817c4d99c4b699
Alessio-windows <alessio.lovato.1@studenti.unipd.it>
2023-03-30 15:15:06 +0200

    Added PCA analysis

--- Lab_1_project/polymer_sorter.m
+++ Lab_1_project/polymer_sorter.m
@@ -19,7 +19,21 @@
     clear sub_matrix array_means;
 
  % Plot curves
-   graphic = plot(scale,data);
+   plot_reflectance = figure(Name="Reflectance");
+   plot_reflectance = plot(scale,data);
    xlabel("Wavelenght [mm]");
    ylabel("Relative reflectance");
    legend('PETE', 'HDPE', 'PVC', 'LDPE', 'PP', 'PS', 'BACKGROUND');
+
+ % Using PCA Analysis
+   [coeff, score, latent, tsqared, exp] = pca(vectors, 'Centered', 'on', 'VariableWeights', 'variance');
+
+ % Plot PCA Analysis
+    plot_PCA = figure(Name="PCA Components");
+    plot_PCA = plot3(score(:, 1), score(:, 2), score(:, 3), 'o','MarkerEdgeColor','b','MarkerFaceColor','b','MarkerSize',4);
+    figure(1);
+    hold;
+    plot(scale, coeff(1:4, :));
+    legend('PETE', 'HDPE', 'PVC', 'LDPE', 'PP', 'PS', 'BACKGROUND', 'PCA COMPONENT 1', 'PCA COMPONENT 2', 'PCA COMPONENT 3', 'PCA COMPONENT 4');
+
+  
